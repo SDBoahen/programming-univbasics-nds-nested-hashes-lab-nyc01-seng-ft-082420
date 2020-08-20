3d5224b1da4b6055723d3fa3ceff2fcd852692a4
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-20 23:29:02 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,22 +13,26 @@ def  assembled_aoh
 # Build an array that contains (or, "nests") the constants into a single (Array).
 
 
-
-
 def 
   
   literal_aoh
   
-  
-    DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
+  literal_consts =
+  [
+    
+  DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
   JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
   PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
   KATE_G = { name:  "Kate Gompert", occupation: "None" }
   BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  
+  ]
 
 end
 # Using Array literal syntax only, build a nested array that uses the data in
 # held in the constants
+
+
 
 
 def aoh_lookup(aoh, row, key)
