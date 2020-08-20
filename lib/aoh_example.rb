0bd09416971758365ffb5a290a1d0b1ad9751aa7
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-20 23:26:39 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,10 +17,7 @@ def
   
   # Build an array that contains (or, "nests") the constants into a single (Array).
   assembled_consts =
-  [
-    DON_G
-      
-  ]
+  [ DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G ]
   
   
 end
