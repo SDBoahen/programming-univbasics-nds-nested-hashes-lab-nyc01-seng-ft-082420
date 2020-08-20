b7537ba974e782ac09471142af8451dd79c50293
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-20 23:30:16 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,10 +20,10 @@ def
   literal_consts =
   [
     
-  { name:  "Don Gately", occupation: "Live-in Staff" }
-  { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-  { name:  "Pat Monteseian", occupation: "Staff" }
-  { name:  "Kate Gompert", occupation: "None" }
+  { name:  "Don Gately", occupation: "Live-in Staff" },
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" },
+  { name:  "Kate Gompert", occupation: "None" },
   { name:  "Bruce Green", occupation: "Fan of Mildred" }
   
   ]
